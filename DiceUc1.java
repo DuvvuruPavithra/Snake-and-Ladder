b74b79b560f72e8572a1caf25f1b3086fc32b2cd
DuvvuruPavithra <duvvurupavithra28@gmail.com>
2022-01-21 22:14:54 +0530

    Added : Adding the Snake and ladder uc1 file

---
+++ DiceUc1.java
@@ -0,0 +1,15 @@
+package Com.BridgeLabz;
+
+public class DiceUc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int startPosition = 0;
+
+		System.out.println("Starting position of Snake and Ladder is : " + startPosition);
+
+	}
+
+}
+
+
